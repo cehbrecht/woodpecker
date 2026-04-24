b952e5cc78c8f50e75007087ca412ec5db9eac1d
Pingu Carsti <ehbrecht@dkrz.de>
2026-04-24 14:23:14 +0200

    clean up imports

--- woodpecker/inout/__init__.py
+++ woodpecker/inout/__init__.py
@@ -1,26 +1,15 @@
-from .base import DataInput, OutputAdapter
-from .detect import collect_netcdf_files, resolve_input, resolve_output_adapter
-from .folder import FolderInput
-from .nc import NetCDFInput, NetCDFOutputAdapter, netcdf_backend_available
+from .base import DataInput
+from .nc import NetCDFInput
 from .normalize import get_output_adapter, normalize_inputs
 from .runtime import get_io_availability
-from .xr import XarrayInput
 from .zarr import ZarrInput, ZarrOutputAdapter
 
 __all__ = [
-    "collect_netcdf_files",
     "DataInput",
-    "FolderInput",
     "get_io_availability",
     "get_output_adapter",
     "NetCDFInput",
-    "NetCDFOutputAdapter",
-    "netcdf_backend_available",
     "normalize_inputs",
-    "OutputAdapter",
-    "resolve_input",
-    "resolve_output_adapter",
-    "XarrayInput",
     "ZarrInput",
     "ZarrOutputAdapter",
 ]
