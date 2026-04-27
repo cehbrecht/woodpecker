09f4ec9609d33469ec566d2c634595ee1a17b33a
Pingu Carsti <ehbrecht@dkrz.de>
2026-04-27 18:53:22 +0200

    renamed inout to io

--- woodpecker/cli.py
+++ woodpecker/cli.py
@@ -11,7 +11,7 @@ import woodpecker.fixes  # noqa: F401
 from woodpecker.execution import run_check, run_fix
 from woodpecker.fixes.registry import FixRegistry
 from woodpecker.formatting import format_findings, format_fix_stats, format_fixes, format_plans
-from woodpecker.inout import get_io_availability
+from woodpecker.io import get_io_availability
 from woodpecker.plans.resolver import RunContext, resolve_load_source_plans, resolve_run_context
 from woodpecker.provenance import build_prov_document, write_prov_document
 from woodpecker.stores.helpers import create_fix_plan_store
